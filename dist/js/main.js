a6ae5dca657d9318cd6d6c85f1e64ea1745fadc8
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-04-19 23:00:56 +0430

    toggle menu 1/2

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,32 @@
 let btn = document.getElementById("btn");
 let light = document.getElementById("light");
-function toggleBtn(){
- btn.classList.toggle("active");
- light.classList.toggle("on");
+function toggleBtn() {
+  btn.classList.toggle("active");
+  light.classList.toggle("on");
 }
 
-var menuBtn = document.getElementById("menuBtn");
-var sideNav = document.getElementById("sideNav");
-var menu = document.getElementById("menu");
+const menuBtn = document.querySelector("menu-img");
+const navLi = document.getElementById("nav-li");
+const menu = document.getElementById("menu");
 
-menuBtn.onclick = function(){
-    if(sideNav.style.left == "-250px"){
-        sideNav.style.right = "0";
-    }
-    else{
-        sideNav.style.right = "-250px";
-    }
-}
+// menu.onclick = function a() {
+//   if (navLi.style.left == "-250px") {
+//     navLi.style.right = "0";
+//   } else {
+//     navLi.style.right = "-250px";
+//   }
+// };
 
+// function toggle() {
+//   menu.addEventListener("click", function () {
+//     if (navLi.classList.contains("show-links")) {
+//       navLi.classList.remove("show-links");
+//     } else {
+//       navLi.classList.add("show-links");
+//     }
+//   });
+// }
 
+menu.addEventListener("click", function () {
+  navLi.classList.toggle("show-links");
+});
